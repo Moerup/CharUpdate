c54176284281625cb6625d5d73cbc301b571c2b8
nikolajmorup@gmail.com <71891@grundfos.com>
2018-10-15 14:18:08 +0200

    Formatting

--- PullChars.ps1
+++ PullChars.ps1
@@ -1,29 +1,27 @@
-$Global:PCArrayTrusted =  @( 
-"DK-TC100776", "DK-TC100777", "DK-TC100812", "DK-TC100847", "DK-TC100857",  
-"PD100106", "PD100299", "PD100347", "PW00305", "PW00323", "PW00327", "PW00332", "PW00462", 
-"PW00485", "PW00503", "PW00535", "PW00536", "PW00544", "PW00549", "PW00568", 
-"PW00569", "PW00589", "PW00735", "PW00781", "PW100103", "PW20013", "PX00138", 
-"PX00139", "DK-TC100350","PD100105", "PD100236", "PW00174", "PW00496", "PW00642", "PW00645", "DK-TC100617")
+$Global:PCArrayTrusted = @( 
+    "DK-TC100776", "DK-TC100777", "DK-TC100812", "DK-TC100847", "DK-TC100857",  
+    "PD100106", "PD100299", "PD100347", "PW00305", "PW00323", "PW00327", "PW00332", "PW00462", 
+    "PW00485", "PW00503", "PW00535", "PW00536", "PW00544", "PW00549", "PW00568", 
+    "PW00569", "PW00589", "PW00735", "PW00781", "PW100103", "PW20013", "PX00138", 
+    "PX00139", "DK-TC100350", "PD100105", "PD100236", "PW00174", "PW00496", "PW00642", "PW00645", "DK-TC100617")
 
-$Global:PCArraySQL =  @( 
-"DK-TC100776", "DK-TC100777", "DK-TC100812", "DK-TC100847", "DK-TC100857", 
-"DK-TC101015", "DK-TC101102", "DK-TC101126", "DK-TC101275", "DK-TC101299", 
-"DK-TC101304", "DK-TC101306", "DK-TC101365", "PW00051", "PW00064", "PW00192", "PW20003",
-"PX00068", "PX00112", "PX00113", "PX00114", "PX00115", "DK-TC100350", "PW00496")
+$Global:PCArraySQL = @( 
+    "DK-TC100776", "DK-TC100777", "DK-TC100812", "DK-TC100847", "DK-TC100857", 
+    "DK-TC101015", "DK-TC101102", "DK-TC101126", "DK-TC101275", "DK-TC101299", 
+    "DK-TC101304", "DK-TC101306", "DK-TC101365", "PW00051", "PW00064", "PW00192", "PW20003",
+    "PX00068", "PX00112", "PX00113", "PX00114", "PX00115", "DK-TC100350", "PW00496")
 
 $Global:SqlSelect = "SELECT VarName FROM LaserMgrClassDataCfg"
 $Global:CharArray = @("")
 $Global:Username = get-content $psscriptroot\Passwords.txt | Select-Object -first 1
 $Global:Password = get-content $psscriptroot\Passwords.txt | Select-Object -first 1 -skip 1
 
-ForEach($PC in $Global:PCArraySQL){
-    Try 
-    { 
-    $SqlReturn = @(Invoke-Sqlcmd -ServerInstance $PC -Database "Marking" -Query $Global:SqlSelect -ErrorAction Stop -QueryTimeout 2 -Username $Global:Username -Password $Global:Password) | Select-Object -ExpandProperty VarName
-    $CharArray += $SqlReturn
+ForEach ($PC in $Global:PCArraySQL) {
+    Try { 
+        $SqlReturn = @(Invoke-Sqlcmd -ServerInstance $PC -Database "Marking" -Query $Global:SqlSelect -ErrorAction Stop -QueryTimeout 2 -Username $Global:Username -Password $Global:Password) | Select-Object -ExpandProperty VarName
+        $CharArray += $SqlReturn
     } 
-    catch 
-    {
+    catch {
         Write-Host "SQL Connection failed to $PC. Pinging.."
         $Ping = Test-Connection $PC -Quiet -Count 2
         if ($Ping -eq $True) {
@@ -37,14 +35,12 @@ ForEach($PC in $Global:PCArraySQL){
     }
 }
 Write-Host "--------------- SQL Connections done ---------------------"
-ForEach($PC in $Global:PCArrayTrusted){
-    Try 
-    { 
-    $SqlReturn = @(Invoke-Sqlcmd -ServerInstance $PC -Database "Marking" -Query $Global:SqlSelect -ErrorAction Stop -QueryTimeout 2) |Select-Object -ExpandProperty VarName
-    $CharArray += $SqlReturn
+ForEach ($PC in $Global:PCArrayTrusted) {
+    Try { 
+        $SqlReturn = @(Invoke-Sqlcmd -ServerInstance $PC -Database "Marking" -Query $Global:SqlSelect -ErrorAction Stop -QueryTimeout 2) |Select-Object -ExpandProperty VarName
+        $CharArray += $SqlReturn
     } 
-    catch 
-    {
+    catch {
         Write-Host "Trusted Connection failed to $PC. Pinging.."
         $Ping = Test-Connection $PC -Quiet -Count 2
         if ($Ping -eq $True) {
